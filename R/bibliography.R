6de83f2118775a868ae74468d0d28689de75a9b1
Chris Pritchard <chris@christopherpritchard.co.uk>
2023-12-14 13:43:05 +0000

    Start work on bibliography

--- R/bibliography.R
+++ R/bibliography.R
@@ -1,1 +1,45 @@
 # This holds functions relating to ris file and bibliography manipulation
+
+
+#' importBibliography
+#'
+#' imports a bibliography from a file
+#' uses synthesisr under the hood, so accepts
+#' any filetypes synthesisr will accept
+#'
+#' @param path path to a bibliography file
+#' @param columns the columns to extract
+#' @param keywords whether or not to add keyword column
+#'
+#' @return the bibliography data frame
+#' @examples
+#' bib <- importBibliography("./inst/extdata/test.ris")
+importBibliography <- function(path, columns= c("author", "title", "abstract", "keywords", "Label"), keywords= TRUE){
+    bib <- synthesisr::read_refs(path)
+    cols <- columns
+    bibframe <- data.frame(matrix(ncol = 0, nrow = nrow(b)))
+        for (i in cols){
+            if (i %in% colnames(bib)){
+                bibframe[i] <- gsub("([[:upper:]])", "\\L\\1", bib[,i], perl = TRUE)
+            }
+        }
+    if(keywords==TRUE){
+        bibframe$Keyword_count <- 0
+    }
+    return(bibframe)
+}
+
+#' listUniqueWordsInBibliography
+#'
+#' creates a character vector containing the unique words in a bibliography
+#'
+#' @param bibframe the bibliography dataframe
+#' @param columns the columns to include
+#'
+#' @return a character vector containing the unique words in the bibliography
+#' @examples
+#' unique1 <- listUniqueWordsInBibliography(bibframe)
+listUniqueWordsInBibliography <- function(bibframe, columns= c("title", "abstract", "keywords")){
+    bibframe <- bibframe[columns]
+    bibframe <- unique(unlist(strsplit(cleanText(as.character(bibframe)), "\\s+")))
+}
--- R/builder.R
+++ R/builder.R
@@ -1,1 +1,53 @@
 # This holds functions relating to search string building and manipulation
+
+
+#' initialiseSearchTerms
+#'
+#' Initialise the search terms dataframe
+#'
+#' @return dataframe containing an empty history column
+#' @examples
+#' st <- initialiseSearchTerms()
+initialiseSearchTerms <- function(){
+    frame <- data.frame(History = NULL)
+    return(frame)
+}
+
+#' addSearchTerm
+#'
+#' Adds a search term to the search dataframe
+#'
+#' @param df the dataframe to add the term to
+#' @param term a string containing the term to add
+#'
+#' @return the dataframe with a list added
+#' @examples
+#' searchTerms <- addSearchTerm(searchTerms, "Paramedic AND Ambulance")
+addSearchTerm <- function(df, term){
+    frame <- rbind(data.frame(df, data.frame(History = term, stringsAsFactors = FALSE)))
+    return(frame)
+}
+
+removeSearchTerm <- function(list, id){
+    frame <- list[-as.numeric(id),]
+    return(frame)
+}
+
+replaceSearchTerm <- function(list, id, newTerm){
+    frame <- list[id, "History"] <- as.character(newTerm)
+    return(frame)
+}
+
+getSearchTerm <- function(list, id){
+    term <- list[id, "History"]
+    return(term)
+}
+
+combineSearchTerms <- function(list, ids){
+    searchString <- paste0("(", list[ids], collapse = " OR ", ")")
+    return(searchString)
+}
+
+makeCorpusSearchString <- function(searchString){
+
+}
