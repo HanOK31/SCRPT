a4fdc55c8e84cf7513907a51cf14314f8ecba374
HanOK31 <153728221+HanOK31@users.noreply.github.com>
2023-12-14 15:12:56 +0000

    addition of corpus functions

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,35 @@
 # This holds functions that are shared across the package
 
 formatSearchString <- function(list){
-    list <- gsub("^\"", "<",list)
-    list <-  gsub("\"$", ">", list)
-    list <- gsub("(.*)( NEAR/)([[:digit:]])(.*)", '"\\1\\4\\\"~3', list)
+    list <- gsub("^"", "<",list)
+    list <- gsub(""$", ">", list)
+    list <- gsub("(.*)( NEAR/)([[:digit:]]+)(.*)", '"\\1\\4\\\"~\\3', list)
     list <- paste0("(", list,")")
     substring <- paste0(list, collapse = " OR ")
     return (substring)
 }
 
-runSearchString <- function(substring, frame){
+cleaning <- function(text){
+    a1 <- c("acknowledgments", "acknowledgements", "conflicts of interest", "conflict of interest", "financial contributions", "financial support", "declaration of interest", "supplementary material", "references", "supporting information", "author contributions")
+    a2 <- stri_locate_last(text, regex = a1)
+    a3 <- which.min(a2[,1])
+    a4 <- a1[a3]
+    input <- gsub(paste0("(.*)", a4, ".*$"),"\\1", x)
+    return(input)
+}
+
+makeCorpus <- function(input){
+    ##check text columns - may need adjustign with IF statement
+    textcorpus <- corpustools::create_tcorpus(input, doc_id = 1:length(input), text_columns = c("abstract", "keywords"))
+    textcorpus <- textcorpus$preprocess(remove_stopwords=T)
+    for (i in 1:nrow(textcorpus)){
+        cleaning(i)
+    }
+    return(textcorpus)
+}
+
+runSearchString <- function(substring, textcorpus){
     #corpus tools
-    results <- corpustools::search_context(frame, substring)
+    results <- corpustools::search_contexts(frame, substring)
     return(results$hits)
 }
