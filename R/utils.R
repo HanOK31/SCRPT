c61ffa59fec990975dc84136d74c2721e72f52f6
HanOK31 <153728221+HanOK31@users.noreply.github.com>
2023-12-14 10:38:33 +0000

    Add some utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,15 @@
 # This holds functions that are shared across the package
+
+formatSearchString <- function(list){
+    gsub("^\"", "<",list)
+    gsub("\"$", ">", list)
+    gsub("(.*)( NEAR/)([[:digit:]])(.*)", "\\1\\3\\~([[:digit:]])", list)
+    substring <- paste0(list, collapse = "OR")
+    return (substring)
+}
+
+runSearchString <- function(substring, frame){
+    #corpus tools
+    results <- corpustools::search_context(frame, substring)
+    return(results$hits)
+}
